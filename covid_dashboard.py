af277c5e0ad737e9f23ea3f0c0296aa8fed778a2
Mohamed Sharfan <sharfansaleem72@gmail.com>
2025-08-06 20:29:05 +0530

    updated 3

--- covid_dashboard.py
+++ covid_dashboard.py
@@ -1,19 +1,29 @@
 import pandas as pd
+import streamlit as st
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+
+st.title("COVID-19 Dashboard")
+
 url = "covid_19_clean_complete.csv"
-print("Data loaded successfully")
+st.success("Data loaded successfully!")
 df = pd.read_csv(url, parse_dates=["Date"])
 print(df.head())
 
 print(df.isnull().sum())
 df["Province/State"] = df["Province/State"].fillna("unknown")
-
 df_filtered = df[df["Date"] > "2020-01-01"]
+
+st.subheader("Data Preview")
+st.write(df.head())
+
+
 
 country_total = df_filtered.groupby("Country/Region")["Confirmed"].max().reset_index()
 country_total = country_total.sort_values(by="Confirmed", ascending=False)
+
+st.subheader("Confirmed Cases Barchart")
 
 plt.figure(figsize=(10,6))
 sns.barplot(data = country_total.head(10), x="Country/Region", y="Confirmed")
@@ -22,8 +32,12 @@ plt.xlabel("Country/Region")
 plt.ylabel("Confirmed")
 plt.show()
 
+st.pyplot(plt)
+
 lanka = df[df["Country/Region"] == "Sri Lanka"]
 lanka_grouped = lanka.groupby("Date")["Confirmed"].sum().reset_index()
+
+st.subheader("COVID-19 Confirmed Cases in Sri Lanka")
 
 plt.figure(figsize=(10,6))
 sns.lineplot(data=lanka_grouped, x="Date", y="Confirmed")
@@ -31,3 +45,5 @@ plt.title("COVID-19 Confirmed Cases Trend in Sri Lanka")
 plt.xlabel("Date")
 plt.ylabel("Confimed Cases")
 plt.show()
+
+st.pyplot(plt)
