4c45115ba5d87bc7fffea2f9043cc429b84f87b8
Mohamed Sharfan <sharfansaleem72@gmail.com>
2026-07-14 11:34:00 +0530

    add stats numerical data

---
+++ numerical_data_stats.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+pd.options.display.max_rows = 10
+pd.options.display.float_format = "{:.1f}".format
+
+
+
+
+training_df = pd.read_csv(filepath_or_buffer="https://download.mlcc.google.com/mledu-datasets/california_housing_train.csv")
+
+
+
+print(training_df.describe())
